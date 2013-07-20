e14c9f88a8b83b87e4590aaa5085f8122fdb3f15
Diego Martinez <diego001@gmail.com>
2013-07-20 16:33:50 -0400

    Yup.

--- activist/activist/settings.py
+++ activist/activist/settings.py
@@ -116,7 +116,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sessions',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
