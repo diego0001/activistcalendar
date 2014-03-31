232fa2a94e641af7fe76259817cd0d2b84be3cef
Diego Martinez <diego001@Diegos-MacBook-Pro.local>
2014-03-31 18:23:59 -0400

    Class shuffling. Yay.

--- activist/activistcalendar/models.py
+++ activist/activistcalendar/models.py
@@ -3,26 +3,25 @@ from django import forms
 from django.contrib.auth import models as authmodels
 
 class Profile(authmodels.User):
-    email = models.EmailField(max_length=2048, unique=True)
+    user_email = models.EmailField(max_length=2048, unique=True)
     phone_number = models.CharField(max_length=2048)
     website = models.URLField()
     personal_message = models.TextField()
     date_created = models.DateField(auto_now=True, auto_now_add=True)
 
-class ProfileGroups(models.Model):
-    profile = models.ForeignKey(Profile)
-    group = models.ForeignKey(Group)
+class Group(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    address_line1 = models.TextField()
+    address_line2 = models.TextField(blank=True)
+    city = models.TextField()
+    province = models.TextField()
+    country = models.TextField(default="United States")
+    group_url = models.URLField()
 
-class ProfileInterest(models.Model):
-    profile = models.ForeignKey(Profile)
-    interest = models.ForeignKey(Interest)
-
-class ProfileEvents(models.Model):
-    profile = models.ForeignKey(Profile)
-    event = models.ForeignKey(Event)
-
-class Interests(models.Model):
+class Interest(models.Model):
     interest_name = models.TextField()
+
 
 class Event(models.Model):
     host_group = models.ForeignKey(Group)
@@ -37,13 +36,15 @@ class Event(models.Model):
     event_url = models.URLField()
     description = models.TextField()
 
-class Group(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-    address_line1 = models.TextField()
-    address_line2 = models.TextField(blank=True)
-    city = models.TextField()
-    province = models.TextField()
-    country = models.TextField(default="United States")
-    group_url = models.URLField()
+class ProfileGroups(models.Model):
+    profile = models.ForeignKey(Profile)
+    group = models.ForeignKey(Group)
+
+class ProfileInterest(models.Model):
+    profile = models.ForeignKey(Profile)
+    interest = models.ForeignKey(Interest)
+
+class ProfileEvents(models.Model):
+    profile = models.ForeignKey(Profile)
+    event = models.ForeignKey(Event)
 
