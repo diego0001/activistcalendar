79ad89af6d4d297b1853dc4f5d8637ab872ad56a
Diego Martinez <diego001@Diegos-MacBook-Pro.local>
2014-03-31 18:19:38 -0400

    Final models field on server.

--- activist/activistcalendar/models.py
+++ activist/activistcalendar/models.py
@@ -2,116 +2,48 @@ from django.db import models
 from django import forms
 from django.contrib.auth import models as authmodels
 
-class AvatarPicture(models.Model):
-    location = models.FileField(upload_to='user_pictures/')
+class Profile(authmodels.User):
+    email = models.EmailField(max_length=2048, unique=True)
+    phone_number = models.CharField(max_length=2048)
+    website = models.URLField()
+    personal_message = models.TextField()
+    date_created = models.DateField(auto_now=True, auto_now_add=True)
 
-class Medium (models.Model):
-    location = models.FileField(upload_to='media/')
+class ProfileGroups(models.Model):
+    profile = models.ForeignKey(Profile)
+    group = models.ForeignKey(Group)
 
-class MediumLink (models.Model):
-    media_type = models.CharField(max_length=2048)
-    media = models.ForeignKey(Medium)
-
-class Photo (models.Model):
-    location = models.FileField(upload_to='photos/')
-
-class PhotoLink (models.Model):
-    photo_type = models.CharField (max_length=2048)
-    photo = models.ForeignKey(Photo)
-
-class Interest(models.Model):
-    interest = models.TextField()
-
-class ActivistUser (authmodels.User):
-    nickname = models.CharField(max_length=2048, unique=True)
-    middle_name = models.CharField (max_length=2048)
-    region = models.TextField()
-    address = models.TextField()
-    time_created = models.DateField(auto_now=True, auto_now_add=True)
-    profile = models.TextField()
-    avatar_picture = models.ForeignKey(AvatarPicture)
-
-class ActivistUserInterest (models.Model):
-    user = models.ForeignKey(ActivistUser)
+class ProfileInterest(models.Model):
+    profile = models.ForeignKey(Profile)
     interest = models.ForeignKey(Interest)
 
-class ActivistUserMedia (models.Model):
-    user = models.ForeignKey(ActivistUser)
-    media = models.ForeignKey(MediumLink)
+class ProfileEvents(models.Model):
+    profile = models.ForeignKey(Profile)
+    event = models.ForeignKey(Event)
 
-class ActivistUserPhotos (models.Model):
-    user = models.ForeignKey(ActivistUser)
-    photos = models.ForeignKey(PhotoLink)
+class Interests(models.Model):
+    interest_name = models.TextField()
 
-class Group (models.Model):
-    group_name = models.CharField (max_length=2048, unique=True)
-    group_type = models.CharField (max_length=2048)
-    group_creator = models.ForeignKey(ActivistUser, 
-                                      related_name = 'group_creator', unique=True)
-    group_location = models.TextField()
-    group_region = models.TextField()
-    group_address = models.TextField()
-    group_site = models.URLField()
-    group_phone = models.CharField(max_length=2048)
-    group_email = models.EmailField()
-    group_mission_statement = models.TextField()
-
-class VolunteersGroup(models.Model):
-    user = models.ForeignKey(ActivistUser)
-    group = models.ForeignKey(Group)
-
-class AdministratorGroup(models.Model):
-    user = models.ForeignKey(ActivistUser)
-    group = models.ForeignKey(Group)
-
-class ActivistUserGroup (models.Model):
-    user = models.ForeignKey(ActivistUser)
-    group = models.ForeignKey(Group)
-
-class EventType(models.Model):
-    type_name = models.CharField (max_length=2048)
-
-class Event (models.Model):
-    event_name = models.CharField (max_length=2048, unique=True)
-    event_type = models.CharField (max_length=2048)
-    event_creator = models.ForeignKey(ActivistUser, 
-                                      related_name="event_creator_user")
-    event_host = models.ForeignKey(ActivistUser, 
-                                   related_name="event_host_user")
-    event_type = models.ForeignKey(EventType)
-    event_time = models.TimeField()
-    event_date = models.DateField()
-    location = models.TextField()
-    event_title = models.CharField(max_length=2048)
-    event_purpose = models.TextField()
+class Event(models.Model):
+    host_group = models.ForeignKey(Group)
+    date = models.DateField()
+    time = models.TimeField()
+    address_line1 = models.TextField()
+    address_line2 = models.TextField(blank=True)
+    city = models.TextField()
+    province = models.TextField()
+    country = models.TextField(default="United States")
+    contact_email = models.EmailField(max_length=2048)
+    event_url = models.URLField()
     description = models.TextField()
-    volunteers = models.ForeignKey(ActivistUser, related_name="volunteers")
 
-class GroupEvent(models.Model):
-    group = models.ForeignKey(Group)
-    event = models.ForeignKey(Event)
+class Group(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    address_line1 = models.TextField()
+    address_line2 = models.TextField(blank=True)
+    city = models.TextField()
+    province = models.TextField()
+    country = models.TextField(default="United States")
+    group_url = models.URLField()
 
-class GuestList (models.Model):
-    user_info = models.ForeignKey(ActivistUser)
-    event = models.ForeignKey(Event)
-    rsvp = models.NullBooleanField()
-
-class GroupEvent(models.Model):
-    group = models.ForeignKey(Group)
-    event = models.ForeignKey(Event)
-
-class GroupMedia(models.Model):
-    group = models.ForeignKey(Group)
-    media = models.ForeignKey(MediumLink)
-
-class GroupPhotos(models.Model):
-    group = models.ForeignKey(Group)
-    photo = models.ForeignKey(PhotoLink)
-
-class EventMedia(models.Model):
-    event = models.ForeignKey(Event)
-    media = models.ForeignKey(MediumLink)
-
-class EventPhotos(models.Model):
-    event = models.ForeignKey(Event)
-    photo = models.ForeignKey(PhotoLink)
